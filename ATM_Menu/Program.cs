6d64ee049db8d73b58d026796db8801b54dda6eb
sabarirajp <sabarirajpazhani@gmail.com>
2025-04-28 18:24:40 +0530

    ATM Menu

---
+++ ATM_Menu/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+
+namespace ATM_Menu
+{
+    public class BankDetails
+    {
+        public int AC_Num { get; set; }
+        public string name { get; set; }
+        public int balance { get; set; }
+    }
+    public class Program {
+        public static void checkBalance(int acNum, Hashtable bank)
+        {
+            
+            int balance = 0;
+            if (bank.Contains(acNum))
+            {
+                foreach (DictionaryEntry i in bank)
+                {
+                    BankDetails details = (BankDetails)i.Value;
+
+                    if (i.Key.Equals(acNum))
+                    {
+                        balance = details.balance;
+                        break;
+                    }
+                }
+
+                Console.WriteLine("Your Account Balance : " + balance);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Account Not Found");
+                Console.ResetColor();
+            }   
+        }
+
+        public static void moneyDeposit(int acNum, int dMoney, Hashtable bank)
+        {
+            if(bank.Contains(acNum))
+            {
+                foreach(DictionaryEntry i in bank)
+                {
+                    BankDetails details = (BankDetails)i.Value;
+
+                    if (i.Key.Equals(acNum))
+                    {
+                        bank[i.Key] = details.balance + dMoney;
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Successfully Deposited :)");
+                        Console.ResetColor();
+                        Console.WriteLine(" Bank Balance: A/C :"+details.AC_Num+" : Name: "+ details.name +"Balance : "+details.balance);
+                    }
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Account Not Found");
+                Console.ResetColor();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("========================= ATM Menu =========================");
+            Console.WriteLine("||                           ||                           ||");
+            Console.WriteLine("------------------------------------------------------------");
+            Console.ResetColor();
+
+            Console.WriteLine("                    Choose the Operations                   ");
+            Console.WriteLine("                    1. Check Balance                        ");
+            Console.WriteLine("                    2. Deposit                              ");
+            Console.WriteLine("                    3. Withdraw                             ");
+            Console.WriteLine("                    4. Exist                                ");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("------------------------------------------------------------");
+            Console.ResetColor();
+
+            Hashtable bank = new Hashtable()
+            {
+                {2041, new BankDetails{AC_Num= 2041, name = "Thamizh", balance = 60000} },
+                {2065, new BankDetails{AC_Num= 2065, name = "Sharmila", balance = 70000} },
+                {4034, new BankDetails{AC_Num= 4034, name = "Subha", balance = 90000}  },
+                {5621, new BankDetails{AC_Num= 5621, name = "Sabari", balance = 50000}  }
+            };
+
+           
+            
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Enter the Choice : ");
+                Console.ResetColor();
+                int choice = int.Parse(Console.ReadLine());
+                if (choice == 4)
+                {
+                    Console.WriteLine("Thank You !!");
+                    break;
+                }
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("You Enter 1 for Check the Bank Balance");
+                        Console.Write("Enter the Account Number A/C: ");
+                        int acNum = int.Parse(Console.ReadLine());
+                        checkBalance(acNum, bank);
+
+                        break;
+
+                    case 2:
+                        Console.WriteLine("You Enter 2 for Deposit the Money in Bank");
+
+                        Console.Write("Enter the Account Number: ");
+                        int acNum1 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Enter the Amount for Deposit");
+                        int dMoney = int.Parse(Console.ReadLine());
+
+                        moneyDeposit(acNum1, dMoney, bank);
+
+                        break;
+
+                }
+            }
+        }
+    }
+}
--- tasks/Calculator_with_Type_Conversion.cs
+++ tasks/Calculator_with_Type_Conversion.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Security.Cryptography;
+using System.Text.Json.Serialization.Metadata;
 using System.Transactions;
 using System.Xml.Serialization;
 using static System.Runtime.InteropServices.JavaScript.JSType;
@@ -205,10 +208,225 @@ namespace taks
             Console.ResetColor();
         }
 
+        public static void MiniSuperMarket()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("------------------ Grovery Bill Calculator ------------------");
+            Console.WriteLine("||     ||    ||     ||    ||     ||     ||    ||     ||    ||");
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.ResetColor();
+
+            Console.WriteLine("              Choose the Items for purchasing                 ");
+            Console.WriteLine("              101. Milk              - Rs.20                  ");
+            Console.WriteLine("              102. Coconut Oil       - Rs.50                  ");
+            Console.WriteLine("              103. Sugar             - Rs.15                  ");
+            Console.WriteLine("              104. Salt              - Rs.20                  ");
+            Console.WriteLine("              105. Rice              - Rs.50                  ");
+            Console.WriteLine("              106. Butter            - Rs.30                  ");
+
+            Console.WriteLine("                    For Billing Press '1'                  ");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("____________________________________________________________");
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.ResetColor();
+
+            Dictionary<int, int> itemsPrice = new Dictionary<int, int>();
+            itemsPrice.Add(101, 20);
+            itemsPrice.Add(102, 50);
+            itemsPrice.Add(103, 15);
+            itemsPrice.Add(104, 20);
+            itemsPrice.Add(105, 50);
+            itemsPrice.Add(106, 30);
+
+            Dictionary<int, string> itemsName = new Dictionary<int, string>()
+            {
+                {101, "Milk" },
+                {102, "Coconut Oil" },
+                {103, "Sugar" },
+                {104, "Salt" },
+                {105, "Rice" },
+                {106,"Butter" }
+            };
+
+            Dictionary<string, int> produts = new Dictionary<string, int>();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Here You can Enter the code of Items for purchasing");
+            Console.ResetColor();
+
+            int quantity = 0;
+            int total = 0;
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Enter the Item Code / Proceed Bill Enter '1': ");
+                Console.ResetColor();
+
+                int code = int.Parse(Console.ReadLine());   
+
+                if(code == 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Thank You For Purchasing");
+                    Console.ResetColor();
+                    break;
+                }
+
+                switch (code)
+                {
+                    case 101:
+                        quantity += 1;
+                        Console.Write($"Enter the Quantity of {itemsName[code]} : ");
+                        int quantity1 = int.Parse(Console.ReadLine());
+                        int totalPrice1 = itemsPrice[code] * quantity1;
+                        total += totalPrice1;
+                        produts.Add(itemsName[code], totalPrice1);
+                        break;
+
+                    case 102:
+                        quantity += 1;
+                        Console.Write($"Enter the Quantity of {itemsName[code]} : ");
+                        int quantity2 = int.Parse(Console.ReadLine());
+                        int totalPrice2 = itemsPrice[code] * quantity2;
+                        total += totalPrice2;
+                        produts.Add(itemsName[code], totalPrice2);
+                        break;  
+
+                    case 103:
+                        quantity += 1;
+                        Console.Write($"Enter the Quantity of {itemsName[code]} : ");
+                        int quantity3 = int.Parse(Console.ReadLine());
+                        int totalPrice3 = itemsPrice[code] * quantity3;
+                        total += totalPrice3;
+                        produts.Add(itemsName[code], totalPrice3);
+                        break;
+
+                    case 104:
+                        quantity += 1;
+                        Console.Write($"Enter the Quantity of {itemsName[code]} : ");
+                        int quantity4 = int.Parse(Console.ReadLine());
+                        int totalPrice4 = itemsPrice[code] * quantity4;
+                        total += totalPrice4;
+                        produts.Add(itemsName[code], totalPrice4);
+                        break;
+
+                    case 105:
+                        quantity += 1;
+                        Console.Write($"Enter the Quantity of {itemsName[code]} : ");
+                        int quantity5 = int.Parse(Console.ReadLine());
+                        int totalPrice5 = itemsPrice[code] * quantity5;
+                        total += totalPrice5;
+                        produts.Add(itemsName[code], totalPrice5);
+                        break;
+
+                    case 106:
+                        quantity += 1;
+                        Console.Write($"Enter the Quantity of {itemsName[code]} : ");
+                        int quantity6 = int.Parse(Console.ReadLine());
+                        int totalPrice6 = itemsPrice[code] * quantity6;
+                        total += totalPrice6;
+                        produts.Add(itemsName[code], totalPrice6);
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("You Entered Worng Item Code");
+                        Console.ResetColor();
+                        break;
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.ResetColor();
+            Console.WriteLine("Purchased Items");
+            Console.WriteLine("\tItems\t\tQuantity Cost");
+            foreach (KeyValuePair<string, int> i in produts)
+            {
+                Console.WriteLine($"\t{i.Key}\t\t{i.Value}");
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("--------------------Total Quantity - " + quantity + "---------------------");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("------------------- Total Amount - " + (decimal)total + " -----------------------");
+            Console.ResetColor();
+        }
+
+
+
+
+
+
+
+        public static void ATM_Menu()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("========================= ATM Menu =========================");
+            Console.WriteLine("||                           ||                           ||");
+            Console.WriteLine("------------------------------------------------------------");
+            Console.ResetColor();
+
+            Console.WriteLine("                    Choose the Operations                   ");
+            Console.WriteLine("                    1. Check Balance                        ");
+            Console.WriteLine("                    2. Deposit                              ");
+            Console.WriteLine("                    3. Withdraw                             ");
+            Console.WriteLine("                    4. Exist                                ");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("------------------------------------------------------------");
+            Console.ResetColor();
+
+            Hashtable bank = new Hashtable()
+            {
+                {2041, new BankDetails{AC_Num= 2041, name = "Thamizh", balance = 60000} },
+                {2065, new BankDetails{AC_Num= 2065, name = "Sharmila", balance = 70000} },
+                {4034, new BankDetails{AC_Num= 4034, name = "Subha", balance = 90000}  },
+                {5621, new BankDetails{AC_Num= 5621, name = "Sabari", balance = 50000}  }
+            };
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Enter the Choice : ");
+            Console.ResetColor();
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("You Enter 1 for Check the Bank Balance");
+                    Console.Write("Enter the Account Number A/C: ");
+                    int acNum = int.Parse(Console.ReadLine());
+                    checkBalance();
+
+            }
+        }
+
+        public static void checkbalance()
+        {
+
+        }
+
+
+
+
+
+
+
+
         static void Main(string[] args)
         {
             //Calculator();
-            SecondLargesNumber();
+            //SecondLargesNumber();
+            //MiniSuperMarket();
+            ATM_Menu();
+        }
+
+        public class BankDetails
+        {
+            public int AC_Num { get; set; }
+            public string name { get; set; }
+            public int balance { get; set; }
         }
     }
 }
